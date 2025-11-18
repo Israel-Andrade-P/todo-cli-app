93b88546cb6bae4e342c2216d7f49c1fb6dafdfc
zel <zel88andra@gmail.com>
2025-11-18 17:55:45 -0300

    delete command added

---
+++ cmd/delete.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Israel-Andrade-P/todo-cli-app.git/todo"
+	"github.com/spf13/cobra"
+)
+
+// deleteCmd represents the delete command
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Deletes a task",
+	Long:  `Deletes a task from your list by ID.\nEx: delete <task ID>`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("Provide an task ID to delete.")
+			return
+		}
+		message, err := todo.Delete(args[0])
+		if err != nil {
+			log.Fatalf("error ERR: %v", err)
+		}
+		fmt.Println(message)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deleteCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- todo/todo.go
+++ todo/todo.go
@@ -60,29 +60,14 @@ func AddTodo(body string) error {
 
 func ListAll() error {
 	path := "/home/zel/Go/todo-cli-app/todos.csv"
-	f, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-
-	defer f.Close()
-
-	reader := csv.NewReader(f)
-
-	rows, err := reader.ReadAll()
-	if err != nil {
-		return err
-	}
-	var todos []Todo
-	for _, row := range rows {
-		todos = append(todos, Todo{ID: row[0], Task: row[1], Status: row[2], CreatedAt: row[3]})
-	}
-
 	writer := tabwriter.NewWriter(os.Stdout, 0, 2, 4, ' ', 0)
 
+	todos, err := getTodos(path)
+	if err != nil {
+		return err
+	}
 	layout := "2006-01-02 15:04:05"
-
-	for _, todo := range todos[1:] {
+	for _, todo := range todos {
 		loc := time.Now().Location()
 		t, err := time.ParseInLocation(layout, todo.CreatedAt, loc)
 		if err != nil {
@@ -92,6 +77,43 @@ func ListAll() error {
 	}
 	defer writer.Flush()
 	return nil
+}
+
+func Delete(id string) (string, error) {
+	path := "/home/zel/Go/todo-cli-app/todos.csv"
+	message := ""
+	todos, err := getTodos(path)
+	if err != nil {
+		return message, err
+	}
+	deleted := false
+	for i := 0; i < len(todos); i++ {
+		if todos[i].ID == id {
+			deleted = true
+			todos = append(todos[:i], todos[i+1:]...)
+		}
+	}
+
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return message, err
+	}
+
+	writer := csv.NewWriter(f)
+
+	defer writer.Flush()
+
+	writer.Write([]string{"ID", "Task", "Status", "Created"})
+	for _, todo := range todos {
+		writer.Write([]string{todo.ID, todo.Task, todo.Status, todo.CreatedAt})
+	}
+	if !deleted {
+		message = fmt.Sprintf("Task with ID %s doesn't exist.", id)
+		return message, nil
+	}
+	message = "Task deleted!"
+
+	return message, nil
 }
 
 func getId(path string) (int, error) {
@@ -108,4 +130,25 @@ func getId(path string) (int, error) {
 		return 0, err
 	}
 	return len(rows), nil
+}
+
+func getTodos(path string) ([]Todo, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	var todos []Todo
+	for _, row := range rows[1:] {
+		todos = append(todos, Todo{ID: row[0], Task: row[1], Status: row[2], CreatedAt: row[3]})
+	}
+	return todos, nil
 }
